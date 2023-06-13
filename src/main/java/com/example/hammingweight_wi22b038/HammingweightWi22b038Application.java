869f763a217aea7089a2e67618a36cb1066d6a39
SirHaze <114669880+SirHaze@users.noreply.github.com>
2023-06-13 11:28:20 +0200

    first commit

---
+++ src/main/java/com/example/hammingweight_wi22b038/HammingweightWi22b038Application.java
@@ -0,0 +1,13 @@
+package com.example.hammingweight_wi22b038;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HammingweightWi22b038Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HammingweightWi22b038Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hammingweight_wi22b038/HammingweightWi22b038ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hammingweight_wi22b038;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HammingweightWi22b038ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
