0fc7128189c51d3102ed8e150be830255d51fcad
SirHaze <114669880+SirHaze@users.noreply.github.com>
2023-06-13 12:38:07 +0200

    Added feature that Returns the last String (with Unit Test)

--- src/main/java/com/example/hammingweight_wi22b038/HammingtonController.java
+++ src/main/java/com/example/hammingweight_wi22b038/HammingtonController.java
@@ -1,5 +1,6 @@
 package com.example.hammingweight_wi22b038;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +8,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HammingtonController {
 
+    public static String lastMessage = "Test";
+
+    @GetMapping("/api/hamming/pre")
+    public static String returnPrevious(){
+        return lastMessage;
+    }
+
+
     @RequestMapping("/api/hamming")
     public static int hammingWeight(@RequestParam String input) {
         int weight = calculateHammingWeight(input);
+        lastMessage = input;
         return weight;
     }
 
